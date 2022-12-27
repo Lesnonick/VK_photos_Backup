4c4f93655fc4026cf78c7f8ae13576349ac83534
Nikolai Seliverstov <seliverstov.nd@phystech.edu>
2022-12-27 20:00:04 +0300

    Prereleased version

--- BackupVK.py
+++ BackupVK.py
@@ -1,7 +1,14 @@
 import requests
 import settings
+import logging
+import datetime
 from pprint import pprint
-import json
+
+
+def Unixdate_to_date(Unixtime):
+    date = datetime.datetime.fromtimestamp(Unixtime)
+    return date.strftime('%Y-%m-%d_%H.%M.%S')
+
 
 class VkDownload:
     def __init__(self, version, token, id):
@@ -33,16 +40,81 @@ class VkDownload:
             'v': self.version,
             'album_id': 'profile',
             'extended': '1'
-            #'count': 59
         }
         res = requests.get(URL, params=params).json()
         items = res['response']['items']
-        url_base = []
+        url_base = {}
         for item in items:
+            likes = item['likes']['count']
+            date = Unixdate_to_date(item['date'])
             sizes = sorted(item['sizes'], key=self.sort_size)
-            url_base.append(sizes[-1]['url'])
+            url_base[sizes[-1]['url']] = likes, date
         return url_base
 
+
+class YaUploader:
+    base_host = 'https://cloud-api.yandex.net/'
+
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json/',
+            'Authorization': self.token
+        }
+
+    def upload_from_internet(self, url, yandex_path):
+        uri = 'v1/disk/resources/upload/'
+        request_url = self.base_host + uri
+        params = {
+            'url': url,
+            'path': yandex_path
+        }
+        response = requests.post(request_url, params=params, headers=self.get_headers())
+
+    def new_folder(self, yandex_path, folder_name):
+        uri = 'v1/disk/resources/'
+        request_url = self.base_host + uri
+        folder_path = yandex_path + folder_name
+        params = {
+            'path': folder_path
+        }
+        requests.put(request_url, params=params, headers=self.get_headers())
+        return folder_path
+
+    def rename_file(self, yandex_path, init_name, name):
+        uri = 'v1/disk/resources/move/'
+        request_url = self.base_host + uri
+        file_path = yandex_path + '/' + init_name
+        new_file_path = yandex_path + '/' + name
+        params = {
+            'from': file_path,
+            'path': new_file_path
+        }
+        requests.post(request_url, params=params, headers=self.get_headers())
+
+
+download_logger = logging.getLogger(__name__)
+download_logger.setLevel(logging.INFO)
+download_handler = logging.FileHandler(f"{__name__}.log", mode = "w")
+download_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+download_handler.setFormatter(download_formatter)
+download_logger.addHandler(download_handler)
+
 if __name__ == '__main__':
-    vkD = VkDownload(settings.version, settings.token, settings.id)
-    print(vkD.get_VK_profile_photos())
+    vk = VkDownload(settings.version, settings.vk_token, settings.id)
+    url_base = vk.get_VK_profile_photos()
+    ya = YaUploader(settings.ya_TOKEN)
+    folder_path = ya.new_folder('', '/id' + str(settings.id) + '_VK_backup')
+    # ya.rename_file('', '1.jpg', '2.jpg')
+    likes_number = []
+    for key, value in url_base.items():
+        likes, data = value[0], value[1]
+        if likes in likes_number:
+            ya.upload_from_internet(key, f'{folder_path}/{likes}_{data}.jpg')
+            ya.rename_file(folder_path, f'{likes}.jpg', f'{likes}_{698}.jpg')
+        else:
+            ya.upload_from_internet(key, f'{folder_path}/{likes}.jpg')
+            likes_number.append(likes)
+        download_logger.info(f"The image was moved to the Ya.Disk folder by the link: {key}")
--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 import os
 
 id = '138597392'
-token = 'vk1.a.R34l1g01Hco9Ag-oWE_IeHV_1pBQUEswq6pn98fJTaG8C-oIKsRnuIplDOIg92NqlW6lZfBIvmfk1VMuU9x_kea0LirLabGcGEKAHQ-de3mDrnA_e7XYHU3MAbYsFcSmA0L_lvgSMAInN49_PFh9OuwHTk899glfKZ7oUa5J6I86_W_Nh71Q_Hd0gK_0wKUOG7jPaJse8QL3vWpCiLkx_A'
+vk_token = ''
 version = '5.131'
 
-TOKEN = ''
+ya_TOKEN = ''
 script_dir = os.getcwd()
 file_dir = os.path.join(script_dir, 'file_to_YA')
 path_to_file = os.path.join(file_dir, 'foto_football.jpg')
